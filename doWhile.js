483b73a95de6563b42ba8bde34b61f29ba54e467
Caio Martins do Nascimento <martinscaio29@gmail.com>
2023-06-22 18:54:37 -0300

    moreExer

--- doWhile.js
+++ doWhile.js
@@ -1,12 +1,23 @@
-function positiveSum(arr){
+function positiveSum(arr) {
     let sum = 0;
-    for(let i=0; i < arr.length; i++){
-        if(arr[i] >0){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
             sum += arr[i];
         }
     }
     return sum;
 }
 
-console.log(positiveSum([1,2,3,4,5]));
+//console.log(positiveSum([1,2,3,4,5]));
+
+
+
+function makeNegative(number) {
+    if (number > 0) {
+        return number * -1;
+    }
+    return number;
+}
+
+//console.log(makeNegative(0.12));
 
